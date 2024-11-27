b9f36df5dceb1eb66642f8ca72b307cc6ba517fc
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-11-27 22:20:28 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExampleComponent from "./components/ExampleComponent";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <ExampleComponent />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ExampleComponent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ExampleComponent = () => {
+  return (
+    <div className="p-6 bg-green-300 rounded-md shadow-md">
+      <h2 className="text-2xl font-semibold">This is a Test Component</h2>
+      <p className="mt-2 text-lg">
+        Tailwind CSS is working if you see this styled text!
+      </p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
