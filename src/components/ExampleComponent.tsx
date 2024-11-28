35861a816862156c2444327348799d09fcd3f985
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-11-28 16:27:49 +0100

    tailwind config error

--- src/components/ExampleComponent.tsx
+++ src/components/ExampleComponent.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-
 const ExampleComponent = () => {
   return (
-    <div className="p-6 bg-green-300 rounded-md shadow-md">
-      <h2 className="text-2xl font-semibold">This is a Test Component</h2>
-      <p className="mt-2 text-lg">
+    <div className="p-6 bg-green-600 rounded-md shadow-lg">
+      <h2 className="text-2xl font-bold">This is a Test Component</h2>
+      <p className="mt-7 text-lg">
         Tailwind CSS is working if you see this styled text!
       </p>
+      <h1 className="mt-2 bg-red-800">HEYOOO</h1>
     </div>
   );
 };
